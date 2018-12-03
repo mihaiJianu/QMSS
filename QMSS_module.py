21cb5c053691e1dc71c73c939e55f59f0d27a5b2
mihaiJianu <45464636+mihaiJianu@users.noreply.github.com>
2018-12-03 14:49:46 +0100

    Aggiornato

--- QMSS_module.py
+++ QMSS_module.py
@@ -1,6 +1,7 @@
 from random import *
 from selection.Selection import partitionDet, quickSelectRand, quickSelectDet
 
+
 def quickSelectSort(l, select):
     assert type(l) == list, "Error! Not a list"
 
@@ -11,39 +12,35 @@ def recursiveQuickSelectSort(l, left, right, select):
     if left >= right:
         return
 
-    if select == 0:
-        pivot = sampleMedianSelect(l[left : right + 1])
-    
-    # se pivot == mediano velocizza l'esecuzione di quicksort
-    
-    elif select == 1:
-        pivot = quickSelectRand(l, int(len(l) / 2))
-    
-    # lista minima di 10 elementi, altrimenti conviene ordinarli per estrarre il mediano
+    if (select == 0):
+        pivot = sampleMedianSelect(l[left: right + 1])
 
-    elif select == 2:
-        pivot = quickSelectDet(l, int(len(l) / 2), 10)
+    elif (select == 1):
+        pivot = quickSelectRand(l, int((left + right) / 2)+1)
 
-    #print(pivot)
-    #print("({},{})".format(left, right))
+    elif (select == 2):
+        pivot = quickSelectDet(l, int((left + right) / 2)+1, 10)
+
+    # print(pivot)
+    # print("({},{})".format(left, right))
     pIndex = partitionDet(l, left, right, pivot)
-    #print(l)
+    # print(l)
     recursiveQuickSelectSort(l, left, pIndex - 1, select)
-    #print("({},{})".format(left, right))
-    #print(l)
+    # print("({},{})".format(left, right))
+    # print(l)
     recursiveQuickSelectSort(l, pIndex + 1, right, select)
-    #print("({},{})".format(left, right))
-    #print(l)
+    # print("({},{})".format(left, right))
+    # print(l)
 
 
 def sampleMedianSelect(l):
     """
-    #@param l: list 
+    #@param l: list
     #@return pivot: int
 
     #Si assuma che m sia uguale a 5
     #Costruisco l'insieme V di 5 elementi scelti a caso da l
-    
+
     """
     m = 5
     i = 0
@@ -51,14 +48,14 @@ def sampleMedianSelect(l):
     temp = l.copy()
 
     while i < m and len(temp) != 0:
-        lenTemp = len(temp) 
+        lenTemp = len(temp)
         index = randint(0, lenTemp - 1)
         V.append(temp.pop(index))
         i += 1
 
-    #Effettuo un selection sort sui primi len(V)/2 elementi.
-    #L'elemento alla posizione len(V) / 2 elemento sarà il mediano di V
-    
+    # Effettuo un selection sort sui primi len(V)/2 elementi.
+    # L'elemento alla posizione len(V) / 2 elemento sarà il mediano di V
+
     for k in range(0, int(len(V) / 2)):
 
         min_pos = k
@@ -69,7 +66,5 @@ def sampleMedianSelect(l):
         V[min_pos], V[k] = V[k], V[min_pos]  # m
 
     pivot = V[int(len(V) / 2)]
-    return pivot
-
-    
+    return pivot   
 
