201b6468a47008f50d39ac78c7f757a8339ccc18
Daniele La Prova <32265040+Torkin1@users.noreply.github.com>
2018-12-03 14:23:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,47 +11,54 @@ if __name__ == "__main__":
 
     temp = l.copy()
 
-    parser = argparse.ArgumentParser("oython - main", description='profile execution time, of major sortin algorithms')
+    parser = argparse.ArgumentParser("python -m main", description='profile execution time, of major sortin algorithms')
     parser.add_argument("sel", type = str, choices = ["med", "rand", "det"], help =  "choose the type of selection used by quickSort")
     args = parser.parse_args()
 
-    if(args.sel == "med"):
+    if args.sel == "med":
 
         cProfile.run('QMSS_module.quickSelectSort(l, 0)', 'stats.txt')
         pstats.Stats('stats.txt').strip_dirs().sort_stats("time").print_stats()
 
-    elif(args.sel == "rand"):
+    elif args.sel == "rand":
 
         cProfile.run('QMSS_module.quickSelectSort(l, 1)', 'stats.txt')
         pstats.Stats('stats.txt').strip_dirs().sort_stats("time").print_stats()
 
-    elif(args.sel == "det"):
+    elif args.sel == "det":
 
         cProfile.run('QMSS_module.quickSelectSort(l, 2)', 'stats.txt')
         pstats.Stats('stats.txt').strip_dirs().sort_stats("time").print_stats()
-
     l = temp.copy()
+    
     cProfile.run('selectionSort(l)', 'stats.txt')
     pstats.Stats('stats.txt').strip_dirs().sort_stats("time").print_stats()
     l = temp.copy()
+    
     cProfile.run('insertionSortDown(l)', 'stats.txt')
     pstats.Stats('stats.txt').strip_dirs().sort_stats("time").print_stats()
     l = temp.copy()
+    
     cProfile.run('bubbleSort(l)', 'stats.txt')
     pstats.Stats('stats.txt').strip_dirs().sort_stats("time").print_stats()
     l = temp.copy()
+    
     cProfile.run('mergeSort(l)', 'stats.txt')
     pstats.Stats('stats.txt').strip_dirs().sort_stats("time").print_stats()
     l = temp.copy()
+    
     cProfile.run('quickSort(l)', 'stats.txt')
     pstats.Stats('stats.txt').strip_dirs().sort_stats("time").print_stats()
     l = temp.copy()
+    
     cProfile.run('heapSort(l)', 'stats.txt')
     pstats.Stats('stats.txt').strip_dirs().sort_stats("time").print_stats()
     l = temp.copy()
+    
     cProfile.run('radixSort(l, 100, 10)', 'stats.txt')
     pstats.Stats('stats.txt').strip_dirs().sort_stats("time").print_stats()
     l = temp.copy()
+    
     cProfile.run('l.sort()', 'stats.txt')
     pstats.Stats('stats.txt').strip_dirs().sort_stats("time").print_stats()
     l = temp.copy()
